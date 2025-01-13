734be204455a30245a1db8ad482338140f23dded
Karim Ahmed <devKarimAhmed@gmail.com>
2025-01-13 05:10:04 +0200

    add admin part 2

---
+++ lib/features/add_admin/cubit/cubit/add_admin_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:our_market_admin/core/api_services.dart';
+
+part 'add_admin_state.dart';
+
+class AddAdminCubit extends Cubit<AddAdminState> {
+  AddAdminCubit() : super(AddAdminInitial());
+
+  final ApiServices _apiServices = ApiServices();
+
+  Future<void> createAnAccount(Map<String, dynamic> data) async {
+    emit(AddAdminLoading());
+    try {
+      emit(AddAdminLoading());
+      Response response = await _apiServices.createAnAccount("signup", data);
+      if (response.statusCode == 200) {
+        emit(AddAdminSuccess());
+      } else {
+        emit(AddAdminError(msgError: response.data["msg"]));
+      }
+    } catch (e) {
+      print(e.toString());
+      emit(AddAdminError(msgError: "Something went wrong , please try again"));
+    }
+  }
+}
--- lib/features/add_admin/views/add_admin_view.dart
+++ lib/features/add_admin/views/add_admin_view.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:our_market_admin/core/components/custom_circle_indicator.dart';
 import 'package:our_market_admin/core/components/custom_elevated_button.dart';
 import 'package:our_market_admin/core/components/custom_text_field.dart';
 import 'package:our_market_admin/core/functions/build_custom_app_bar.dart';
+import 'package:our_market_admin/core/functions/navigate_to.dart';
+import 'package:our_market_admin/core/functions/navigate_without_back.dart';
+import 'package:our_market_admin/core/functions/show_msg.dart';
+import 'package:our_market_admin/features/add_admin/cubit/cubit/add_admin_cubit.dart';
+import 'package:our_market_admin/features/auth/view/login_view.dart';
 
 class AddAdminView extends StatefulWidget {
   const AddAdminView({super.key});
@@ -18,37 +25,61 @@ class _AddAdminViewState extends State<AddAdminView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildCustomAppBar(context, "Add Admin"),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              
-              const SizedBox(
-                height: 20,
-              ),
-              CustomField(
-                labelText: "Email",
-                controller: _emailController,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomField(
-                labelText: "Password",
-                isPassword: true,
-                controller: _passwordController,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              CustomElevatedButton(
-                child: const Text("Add"),
-                onPressed: () {},
-              ),
-            ],
-          ),
+      body: BlocProvider(
+        create: (context) => AddAdminCubit(),
+        child: BlocConsumer<AddAdminCubit, AddAdminState>(
+          listener: (context, state) {
+            if (state is AddAdminSuccess) {
+              navigateWithoutBack(context, const LoginView());
+            }
+            if (state is AddAdminError) {
+              showMsg(context, state.msgError);
+            }
+          },
+          builder: (context, state) {
+            AddAdminCubit cubit = context.read<AddAdminCubit>();
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: state is AddAdminLoading
+                  ? const CustomCircleProgIndicator()
+                  : Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          CustomField(
+                            labelText: "Email",
+                            controller: _emailController,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          CustomField(
+                            labelText: "Password",
+                            isPassword: true,
+                            controller: _passwordController,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          CustomElevatedButton(
+                            child: const Text("Add"),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                cubit.createAnAccount({
+                                  "email": _emailController.text,
+                                  "password": _passwordController.text
+                                });
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+            );
+          },
         ),
       ),
     );
